fbe733ce5a6c3a43e4d998017d427f65761b33c6
M_Ibrokhiiim <ibrokhimmuysinov@gmail.com>
2025-12-23 14:03:27 +0500

    Central error handler middleware implemented to all routes

---
+++ BACK/App2/src/middlewares/err.middleware.ts
@@ -0,0 +1,10 @@
+import { Request,Response,NextFunction } from "express"
+
+export const CentralErrorHandler=(err:any, req:Request, res:Response, next:NextFunction)=>{
+   res.status(err.statusCode || 500).json({
+    success:false, message:err.message || "Internal applicaiton server error", status:err.statusCode
+   })
+}
+
+
+
--- BACK/App2/src/server.ts
+++ BACK/App2/src/server.ts
@@ -1,14 +1,16 @@
 import express from "express"
 import  cors from "cors"
 import router from "./routers/routers.js"
+import { CentralErrorHandler } from "./middlewares/err.middleware.js";
 
 const PORT = 3000;
 const server = express()
 server.use(cors())
 server.use(express.json())
 
-
 server.use('/api',router)
+
+server.use(CentralErrorHandler)
 
 server.listen(PORT,()=>{
     console.log(`Server is working on ${PORT}`)
