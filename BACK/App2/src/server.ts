fa827c91c3d57b66f636afc998633497f2f4d937
M_Ibrokhiiim <ibrokhimmuysinov@gmail.com>
2025-12-23 18:04:59 +0500

    Application ready to presentation

--- BACK/App2/src/server.ts
+++ BACK/App2/src/server.ts
@@ -1,7 +1,6 @@
 import express, { Request, Response, NextFunction } from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-
 import notesRouter from "./routes/notes.router.js";
 import { CentralErrorHandler } from "./middlewares/centralErrorHandler.middleware.js";
 
@@ -17,11 +16,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// ROUTES
+
 app.use("/api", notesRouter);
 
 
-//Works when client sent request to  incorrect endPoint
 app.use((req: Request, res: Response, next: NextFunction) => {
   const error: any = new Error("Route not found");
   error.statusCode = 404;
